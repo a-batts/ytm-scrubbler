766e1ddcd046317b610f8e33a6ce45b9e8acdd8a
Alex Batts <alex.batts05@gmail.com>
2022-01-15 12:31:13 -0500

    Cleaned up formatting of file

--- Scrubbler.js
+++ Scrubbler.js
@@ -4,7 +4,6 @@ const startDate = new Date();
 try {
     let raw = fs.readFileSync('watch-history.json');
     let history = JSON.parse(raw);
-
     let newData = [];
     history.forEach(entry => {
         if (entry.header == 'YouTube Music') {
@@ -12,28 +11,24 @@ try {
             if (entryDate < startDate && entry.subtitles != null) {
                 let artistName = entry.subtitles[0].name.substring(0, entry.subtitles[0].name.indexOf(' - Topic'));
                 let trackName = entry.title.substring(8);
-
                 let newEntry = {
                     'artistName': artistName,
                     'trackName': trackName,
                     'datetime': entry.time,
                 }
-
                 if (artistName && trackName)
                     newData.push(newEntry);
             }
         }
     });
-
     newData = JSON.stringify(newData);
     fs.writeFileSync('scrubbed-watch-history.json', newData);
 
     console.log('Finished scrubbling file!');
 }
 catch (error) {
-    if (error.code === 'ENOENT') {
+    if (error.code === 'ENOENT')
         console.log('File not found!');
-    } else {
+    else
         throw err;
-    }
 }
