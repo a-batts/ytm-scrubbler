916e42299c62bef4b1c8581d2b4e8464edd8aace
Alex Batts <alex.batts05@gmail.com>
2022-01-15 02:17:16 -0500

    Initial commit

---
+++ Scrubbler.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const startDate = new Date();
+
+try {
+    let raw = fs.readFileSync('watch-history.json');
+    let history = JSON.parse(raw);
+
+    let newData = [];
+    history.forEach(entry => {
+        if (entry.header == 'YouTube Music') {
+            entryDate = new Date(entry.time);
+            if (entryDate < startDate && entry.subtitles != null) {
+                let artistName = entry.subtitles[0].name.substring(0, entry.subtitles[0].name.indexOf(' - Topic'));
+                let trackName = entry.title.substring(8);
+
+                let newEntry = {
+                    'artistName': artistName,
+                    'trackName': trackName,
+                    'datetime': entry.time,
+                }
+
+                newData.push(newEntry);
+            }
+        }
+    });
+
+    newData = JSON.stringify(newData);
+    fs.writeFileSync('scrubbed-watch-history.json', newData);
+
+    console.log('Finish scrubbling file!');
+}
+catch (error) {
+    if (error.code === 'ENOENT') {
+        console.log('File not found!');
+    } else {
+        throw err;
+    }
+}
